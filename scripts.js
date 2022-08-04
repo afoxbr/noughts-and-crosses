7634b0509214ec91642ade68617a36d3e271eef7
afoxbr <antonyrbramwell@gmail.com>
2022-08-04 23:26:22 +0100

    Add player factory

--- scripts.js
+++ scripts.js
@@ -10,11 +10,32 @@ const gameBoard = (() => {
     };
 })();
 
+const player = (name, symbol) => {
+    return {
+        name,
+        symbol,
+    };
+};
+
+const player1 = player("Player 1", "X");
+const player2 = player("Player 2", "O");
+
 const playGame = (() => {
-    document.getElementById("newGame").addEventListener("click", function () {
+    document.getElementById("newGame").addEventListener("click", function() {
         for (i = 0; i < 9; i++) {
-            gameBoard.gridContents[i] = "";
+            gameBoard.gridContents[i] = "k";
         };
         gameBoard.generateBoard();
     });
+    const clickBoard = () => {for (i = 0; i < 9; i++) {
+        document.getElementById([i]).addEventListener("click", function() {
+            gameBoard.gridContents[i] = "k";
+            gameBoard.generateBoard();
+        });
+    }};
+    clickBoard();
+    return {
+        clickBoard
+    };
 })();
+
