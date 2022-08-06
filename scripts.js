b978d3cabb17dd7eb3faddfc9931574d663d16e9
afoxbr <antonyrbramwell@gmail.com>
2022-08-06 11:06:26 +0100

    Add basic functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Below module is what displays game grid
 const gameBoard = (() => {
     const gridContents = ["x", "o", "o", "x", "o", "x", "o", "x", "o"];
     const generateBoard = () => {for (i = 0; i < 9; i++) {
@@ -10,6 +11,7 @@ const gameBoard = (() => {
     };
 })();
 
+//Factory function to make player objects
 const player = (name, symbol) => {
     return {
         name,
@@ -20,22 +22,113 @@ const player = (name, symbol) => {
 const player1 = player("Player 1", "X");
 const player2 = player("Player 2", "O");
 
+//The machinery that makes the game move
 const playGame = (() => {
+    //New game button, empties grid
     document.getElementById("newGame").addEventListener("click", function() {
         for (i = 0; i < 9; i++) {
-            gameBoard.gridContents[i] = "k";
+            gameBoard.gridContents[i] = "";
         };
         gameBoard.generateBoard();
     });
-    const clickBoard = () => {for (i = 0; i < 9; i++) {
-        document.getElementById([i]).addEventListener("click", function() {
-            gameBoard.gridContents[i] = "k";
+    //Below section is core functionality
+    //Couldn't make a loop work, will attempt again later
+    const clickBoard = () => {
+        let lastClick = "X"; 
+        document.getElementById("0").addEventListener("click", function() {
+            if (lastClick == "X") {
+                gameBoard.gridContents[0] = "O";
+                lastClick = "O";
+            } else if (lastClick == "O") {
+                gameBoard.gridContents[0] = "X";
+                lastClick = "X"
+            };
             gameBoard.generateBoard();
         });
-    }};
+        document.getElementById("1").addEventListener("click", function() {
+            if (lastClick == "X") {
+                gameBoard.gridContents[1] = "O";
+                lastClick = "O";
+            } else if (lastClick == "O") {
+                gameBoard.gridContents[1] = "X";
+                lastClick = "X"
+            };
+            gameBoard.generateBoard();
+        });
+        document.getElementById("2").addEventListener("click", function() {
+            if (lastClick == "X") {
+                gameBoard.gridContents[2] = "O";
+                lastClick = "O";
+            } else if (lastClick == "O") {
+                gameBoard.gridContents[2] = "X";
+                lastClick = "X"
+            };
+            gameBoard.generateBoard();
+        });
+        document.getElementById("3").addEventListener("click", function() {
+            if (lastClick == "X") {
+                gameBoard.gridContents[3] = "O";
+                lastClick = "O";
+            } else if (lastClick == "O") {
+                gameBoard.gridContents[3] = "X";
+                lastClick = "X"
+            };
+            gameBoard.generateBoard();
+        });
+        document.getElementById("4").addEventListener("click", function() {
+            if (lastClick == "X") {
+                gameBoard.gridContents[4] = "O";
+                lastClick = "O";
+            } else if (lastClick == "O") {
+                gameBoard.gridContents[4] = "X";
+                lastClick = "X"
+            };
+            gameBoard.generateBoard();
+        });
+        document.getElementById("5").addEventListener("click", function() {
+            if (lastClick == "X") {
+                gameBoard.gridContents[5] = "O";
+                lastClick = "O";
+            } else if (lastClick == "O") {
+                gameBoard.gridContents[5] = "X";
+                lastClick = "X"
+            };
+            gameBoard.generateBoard();
+        });
+        document.getElementById("6").addEventListener("click", function() {
+            if (lastClick == "X") {
+                gameBoard.gridContents[6] = "O";
+                lastClick = "O";
+            } else if (lastClick == "O") {
+                gameBoard.gridContents[6] = "X";
+                lastClick = "X"
+            };
+            gameBoard.generateBoard();
+        });
+        document.getElementById("7").addEventListener("click", function() {
+            if (lastClick == "X") {
+                gameBoard.gridContents[7] = "O";
+                lastClick = "O";
+            } else if (lastClick == "O") {
+                gameBoard.gridContents[7] = "X";
+                lastClick = "X"
+            };
+            gameBoard.generateBoard();
+        });
+        document.getElementById("8").addEventListener("click", function() {
+            if (lastClick == "X") {
+                gameBoard.gridContents[8] = "O";
+                lastClick = "O";
+            } else if (lastClick == "O") {
+                gameBoard.gridContents[8] = "X";
+                lastClick = "X"
+            };
+            gameBoard.generateBoard();
+        });
+    };
     clickBoard();
     return {
-        clickBoard
+        clickBoard,
     };
 })();
 
