8ee07912a41ab06c08a909853ad407d344f7fcf6
afoxbr <antonyrbramwell@gmail.com>
2022-08-06 14:03:14 +0100

    Add tie function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //Below module is what displays game grid
 const gameBoard = (() => {
-    const gridContents = ["", "", "", "", "", "", "", "", ""];
+    const gridContents = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
     const generateBoard = () => {for (i = 0; i < 9; i++) {
         document.getElementById([i]).innerHTML = gridContents[(i)];
     }};
@@ -14,8 +14,10 @@ const gameBoard = (() => {
 //Factory function to make player objects
 //Also keeps track of who is winning
 const player = (name, symbol, domRef) => {
+    let gameWon = false;
     let winCount = 0;
     document.addEventListener("click", () => {
+        document.getElementById(domRef).innerHTML = `Rounds won: ${winCount}`
         if (((gameBoard.gridContents[0] == symbol) && (gameBoard.gridContents[1] == symbol) && (gameBoard.gridContents[2] == symbol))
         || ((gameBoard.gridContents[3] == symbol) && (gameBoard.gridContents[4] == symbol) && (gameBoard.gridContents[5] == symbol))
         || ((gameBoard.gridContents[6] == symbol) && (gameBoard.gridContents[7] == symbol) && (gameBoard.gridContents[8] == symbol))
@@ -26,7 +28,7 @@ const player = (name, symbol, domRef) => {
         || ((gameBoard.gridContents[2] == symbol) && (gameBoard.gridContents[4] == symbol) && (gameBoard.gridContents[6] == symbol))) {
             alert(`${name} wins this round!`);
             for (i = 0; i < 9; i++) {
-                gameBoard.gridContents[i] = "";
+                gameBoard.gridContents[i] = " ";
             };
             gameBoard.generateBoard();
             winCount += 1;
@@ -34,9 +36,16 @@ const player = (name, symbol, domRef) => {
             return (gameWon = true);
         };
         if (winCount == 3) {
-            alert(`${name} wins best-of-three!`)
+            alert(`GAME OVER!! ${name} wins best-of-three!`)
             winCount = 0;
         };
+        if ((gameBoard.gridContents.includes(" ") == false) && (gameWon == false)) {
+            alert("It's a tie!");
+            for (i = 0; i < 9; i++) {
+                gameBoard.gridContents[i] = " ";
+            };
+            gameBoard.generateBoard();
+        }
     });
     return {
         name,
@@ -51,13 +60,13 @@ const player2 = player("Player 2", "O", "win2");
 
 //The machinery that makes the game move
 const playGame = (() => {
-
+    let ai = false;
     //New game button, empties grid, resets winCounts
     document.getElementById("newGame").addEventListener("click", function() {
         player1.winCount = 0;
         player2.winCount = 0;
         for (i = 0; i < 9; i++) {
-            gameBoard.gridContents[i] = "";
+            gameBoard.gridContents[i] = " ";
         };
         gameBoard.generateBoard();
     });
@@ -67,7 +76,7 @@ const playGame = (() => {
     const clickBoard = () => {
         let lastClick = "X"; 
         document.getElementById("0").addEventListener("click", function() {
-            if (gameBoard.gridContents[0] == "") {
+            if (gameBoard.gridContents[0] == " ") {
                 if (lastClick == "X") {
                     gameBoard.gridContents[0] = "O";
                     lastClick = "O";
@@ -81,7 +90,7 @@ const playGame = (() => {
             gameBoard.generateBoard();
         });
         document.getElementById("1").addEventListener("click", function() {
-            if (gameBoard.gridContents[1] == "") {
+            if (gameBoard.gridContents[1] == " ") {
                 if (lastClick == "X") {
                     gameBoard.gridContents[1] = "O";
                     lastClick = "O";
@@ -95,7 +104,7 @@ const playGame = (() => {
             gameBoard.generateBoard();
         });
         document.getElementById("2").addEventListener("click", function() {
-            if (gameBoard.gridContents[2] == "") {
+            if (gameBoard.gridContents[2] == " ") {
                 if (lastClick == "X") {
                     gameBoard.gridContents[2] = "O";
                     lastClick = "O";
@@ -109,7 +118,7 @@ const playGame = (() => {
             gameBoard.generateBoard();
         });
         document.getElementById("3").addEventListener("click", function() {
-            if (gameBoard.gridContents[3] == "") {
+            if (gameBoard.gridContents[3] == " ") {
                 if (lastClick == "X") {
                     gameBoard.gridContents[3] = "O";
                     lastClick = "O";
@@ -123,7 +132,7 @@ const playGame = (() => {
             gameBoard.generateBoard();
         });
         document.getElementById("4").addEventListener("click", function() {
-            if (gameBoard.gridContents[4] == "") {
+            if (gameBoard.gridContents[4] == " ") {
                 if (lastClick == "X") {
                     gameBoard.gridContents[4] = "O";
                     lastClick = "O";
@@ -137,7 +146,7 @@ const playGame = (() => {
             gameBoard.generateBoard();
         });
         document.getElementById("5").addEventListener("click", function() {
-            if (gameBoard.gridContents[5] == "") {
+            if (gameBoard.gridContents[5] == " ") {
                 if (lastClick == "X") {
                     gameBoard.gridContents[5] = "O";
                     lastClick = "O";
@@ -151,7 +160,7 @@ const playGame = (() => {
             gameBoard.generateBoard();
         });
         document.getElementById("6").addEventListener("click", function() {
-            if (gameBoard.gridContents[6] == "") {
+            if (gameBoard.gridContents[6] == " ") {
                 if (lastClick == "X") {
                     gameBoard.gridContents[6] = "O";
                     lastClick = "O";
@@ -165,7 +174,7 @@ const playGame = (() => {
             gameBoard.generateBoard();
         });
         document.getElementById("7").addEventListener("click", function() {
-            if (gameBoard.gridContents[7] == "") {
+            if (gameBoard.gridContents[7] == " ") {
                 if (lastClick == "X") {
                     gameBoard.gridContents[7] = "O";
                     lastClick = "O";
@@ -179,7 +188,7 @@ const playGame = (() => {
             gameBoard.generateBoard();
         });
         document.getElementById("8").addEventListener("click", function() {
-            if (gameBoard.gridContents[8] == "") {
+            if (gameBoard.gridContents[8] == " ") {
                 if (lastClick == "X") {
                     gameBoard.gridContents[8] = "O";
                     lastClick = "O";
